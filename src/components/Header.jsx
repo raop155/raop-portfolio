e36f1375f77498ec2926ffd468e098535cd5d2cd
Ricardo Olarte <raop155@hotmail.com>
2020-11-24 18:01:01 -0500

    Add Header and Hero Styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header id='header'>
+      <div className='container'>
+        <p>R.</p>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section id='hero'>
+      <div className='container flex'>
+        <h1>I'm Ricardo</h1>
+        <p>Fullstack Developer focused on Front-end development</p>
+        <p>
+          <i>
+            Passionate about applying the necessary technologies to improve the UX & quality of the
+            product
+          </i>
+        </p>
+        <button class='button button--primary'>Contact Me</button>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
