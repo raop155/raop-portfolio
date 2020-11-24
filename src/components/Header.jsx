18344904e88740d37ee04b99ecd468c1c75a8970
Ricardo Olarte <raop155@hotmail.com>
2020-11-24 18:50:39 -0500

    Add some styles to Header Nav and Hero Section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,39 @@ import React from 'react';
 const Header = () => {
   return (
     <header id='header'>
-      <div className='container'>
-        <p>R.</p>
+      <div className='container flex'>
+        <a href='#hero'>
+          <p>R.</p>
+        </a>
+        <nav className='flex'>
+          <ul>
+            <li>
+              <a className='nav-item active' href='#hero'>
+                Home
+              </a>
+            </li>
+            <li>
+              <a className='nav-item' href='#about'>
+                About
+              </a>
+            </li>
+            <li>
+              <a className='nav-item' href='projects'>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className='nav-item' href='contact'>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className='toggler'>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </header>
   );
--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section id='hero'>
       <div className='container flex'>
         <h1>I'm Ricardo</h1>
-        <p>Fullstack Developer focused on Front-end development</p>
+        <p>Full-stack Developer focused on Front-end development</p>
         <p>
           <i>
             Passionate about applying the necessary technologies to improve the UX & quality of the
