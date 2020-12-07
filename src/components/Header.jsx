93b03e8271afe23924d74543e185efa1f38cf2be
Ricardo Olarte <raop155@hotmail.com>
2020-12-07 11:21:51 -0500

    Add Header style onScroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,66 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Header = () => {
+  useEffect(() => {
+    const navbarHeight = document.querySelector('#header').clientHeight;
+    const navLinks = document.querySelectorAll('.nav-item');
+    const heroSection = document.querySelector('#hero');
+    const projectsSection = document.querySelector('#projects');
+    const aboutSection = document.querySelector('#about');
+    const contactSection = document.querySelector('#contact');
+
+    const checkScrollPosition = () => {
+      const windowScroll = window.scrollY;
+
+      const heroSectionOffset =
+        heroSection.getBoundingClientRect().top + windowScroll - navbarHeight;
+      const projectsSectionOffset =
+        projectsSection.getBoundingClientRect().top + windowScroll - navbarHeight;
+      const aboutSectionOffset =
+        aboutSection.getBoundingClientRect().top + windowScroll - navbarHeight;
+      const contactSectionOffset =
+        contactSection.getBoundingClientRect().top + windowScroll - navbarHeight - 250;
+
+      navLinks.forEach((navLink) => {
+        navLink.classList.remove('active');
+      });
+
+      // console.log(windowScroll, contactSectionOffset);
+
+      if (windowScroll >= heroSectionOffset && windowScroll < projectsSectionOffset) {
+        navLinks.forEach((navLink) => {
+          if (navLink.getAttribute('href') === '#hero') {
+            navLink.classList.add('active');
+          }
+        });
+      } else if (windowScroll >= projectsSectionOffset && windowScroll < aboutSectionOffset) {
+        navLinks.forEach((navLink) => {
+          if (navLink.getAttribute('href') === '#projects') {
+            navLink.classList.add('active');
+          }
+        });
+      } else if (windowScroll >= aboutSectionOffset && windowScroll < contactSectionOffset) {
+        navLinks.forEach((navLink) => {
+          if (navLink.getAttribute('href') === '#about') {
+            navLink.classList.add('active');
+          }
+        });
+      } else if (windowScroll >= contactSectionOffset) {
+        navLinks.forEach((navLink) => {
+          if (navLink.getAttribute('href') === '#contact') {
+            navLink.classList.add('active');
+          }
+        });
+      }
+    };
+
+    document.addEventListener('scroll', checkScrollPosition);
+
+    return () => {
+      document.removeEventListener('scroll', checkScrollPosition);
+    };
+  }, []);
+
   return (
     <header id='header'>
       <div className='container flex'>
@@ -23,7 +83,7 @@ const Header = () => {
               <a className='nav-item' href='#about'>
                 About
               </a>
-            </li>            
+            </li>
             <li>
               <a className='nav-item' href='#contact'>
                 Contact
