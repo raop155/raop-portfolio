b9b3702be4fcf88d793a8fa921dcc85033e1d637
Ricardo Olarte <raop155@hotmail.com>
2020-12-11 13:05:56 -0500

    Untrack files in .gitignore

--- src/components/projects/ProjectGallery.jsx
+++ src/components/projects/ProjectGallery.jsx
@@ -1,36 +1,20 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const ProjectGallery = (props) => {
   const { id, title, setIsGalleryOpen, gallery } = props;
-  const galleryItems = useRef(null);
-  const currentImageIndex = useRef(0);
-
-  const showGalleryItem = (index) => {
-    // console.log(currentImageIndex.current);
-    galleryItems.current.forEach((item) => {
-      item.classList.add('hide');
-    });
-    galleryItems.current[index] && galleryItems.current[index].classList.remove('hide');
-  };
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const nextImage = () => {
-    if (currentImageIndex.current >= gallery.length - 1) return;
-    currentImageIndex.current++;
-    showGalleryItem(currentImageIndex.current);
+    if (currentImageIndex >= gallery.length - 1) return;
+    setCurrentImageIndex((prev) => prev + 1);
   };
 
   const prevImage = () => {
-    if (currentImageIndex.current <= 0) return;
-    currentImageIndex.current--;
-    showGalleryItem(currentImageIndex.current);
+    if (currentImageIndex <= 0) return;
+    setCurrentImageIndex((prev) => prev - 1);
   };
-
-  useEffect(() => {
-    galleryItems.current = document.querySelectorAll(`.item[data-role="gallery-item-${id}"]`);
-    showGalleryItem(currentImageIndex.current);
-  }, [id]);
 
   return (
     <div className='project-gallery'>
@@ -40,12 +24,19 @@ const ProjectGallery = (props) => {
           <h2 className='title'>{title}</h2>
           <div className='gallery'>
             {gallery?.length > 0 &&
-              gallery.map((item) => (
-                <div className='item' data-role={`gallery-item-${id}`} key={item.image}>
-                  <img src={require(`../../assets/${item.image}`).default} alt={item.image} />
-                  <div className='description'>{item.description}</div>
-                </div>
-              ))}
+              gallery.map((item, index) => {
+                const showImage = currentImageIndex === index;
+                return (
+                  <div
+                    className={showImage ? 'item' : 'item hide'}
+                    data-role={`gallery-item-${id}`}
+                    key={item.image}
+                  >
+                    <img src={require(`../../assets/${item.image}`).default} alt={item.image} />
+                    <div className='description'>{item.description}</div>
+                  </div>
+                );
+              })}
 
             {gallery?.length && (
               <>
