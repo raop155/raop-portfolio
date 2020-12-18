c0860d7ea029a77fa7e5ce1bf1c006bc847affca
Ricardo Olarte <raop155@hotmail.com>
2020-12-18 18:54:29 -0500

    Remove warnings

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -1,6 +1,5 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import DarkModeToggler from '../hero/DarkModeToggler';
-import useIntersectionObserver from '../../hooks/useIntersectionObserver';
 
 const Hero = (_, ref) => {
   return (
